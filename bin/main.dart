fd3c1a426738bce429f8730f667aa71d7c6a5a29
thunder775 <rahulmasih61@gmail.com>
2020-02-19 10:18:32 +0530

    O(n+m) without list

--- bin/main.dart
+++ bin/main.dart
@@ -7,6 +7,10 @@
 //  In this example, assume nodes with the same value are the exact same
 //  node objects.
 
+import 'dart:math';
+
+import 'package:test/test.dart';
+
 class Node {
   int val;
   Node next;
@@ -30,52 +34,92 @@ class Node {
 //  }
 //  return null;
 //}
-/// O(n+m)
+/// O(n+m)(using list)
+//Node getIntersectingNode(Node A, Node B) {
+//  List temp1 = [];
+//  List temp2 = [];
+//  Node a = A;
+//  Node b = B;
+//  while (true) {
+//    temp1.add(a.val);
+//    if (a.next == null) {
+//      break;
+//    }
+//    a = a.next;
+//  }
+//  while (true) {
+//    temp2.add(b.val);
+//    if (b.next == null) {
+//      break;
+//    }
+//    b = b.next;
+//  }
+//  print(temp1);
+//  print(temp2);
+//  int intersectingNodeVal = 0;
+//  if (temp2.last != temp1.last) {
+//    return null;
+//  } else {
+//    int len = temp1.length <= temp2.length ? temp1.length : temp2.length;
+//    for (int i = 1; i <= len; i++) {
+//      if (temp1[temp1.length - i] == temp2[temp2.length - i]) {
+//        continue;
+//      } else {
+//        intersectingNodeVal = temp1[temp1.length - (i - 1)];
+//        break;
+//      }
+//    }
+//  }
+//  a = A;
+//  while (true) {
+//    if (a.val == intersectingNodeVal) {
+//      return a;
+//    }
+//    a = a.next;
+//  }
+//}
+/// O(m+n) without list
 Node getIntersectingNode(Node A, Node B) {
-  List temp1 = [];
-  List temp2 = [];
+  int lengthA = 1;
+  int lenghtB = 1;
   Node a = A;
   Node b = B;
-  while (true) {
-    temp1.add(a.val);
-    if (a.next == null) {
-      break;
-    }
+
+  while (a.next != null) {
+    lengthA++;
     a = a.next;
   }
-  while (true) {
-    temp2.add(b.val);
-    if (b.next == null) {
-      break;
-    }
+  while (b.next != null) {
+    lenghtB++;
     b = b.next;
   }
-  print(temp1);
-  print(temp2);
-  int intersectingNodeVal = 0;
-  if (temp2.last != temp1.last) {
-    return null;
-  } else {
-    int len = temp1.length <= temp2.length ? temp1.length : temp2.length;
-    for (int i = 1; i <= len; i++) {
-      if (temp1[temp1.length - i] == temp2[temp2.length - i]) {
-        continue;
-      } else {
-        intersectingNodeVal = temp1[temp1.length - (i - 1)];
-        break;
-      }
+
+  int greaterLength = max(lengthA, lenghtB);
+  if (greaterLength == lengthA) {
+    for (int i = 0; i < (lenghtB - lengthA).abs(); i++) {
+      A = A.next;
     }
   }
-  a = A;
-  while (true) {
-    if (a.val == intersectingNodeVal) {
-      return a;
+  if (greaterLength == lenghtB) {
+    for (int i = 0; i < (lenghtB - lengthA).abs(); i++) {
+      B = B.next;
     }
-    a = a.next;
+  }
+
+  while (true) {
+    if (A.val == B.val) {
+      return A;
+    }
+    A = A.next;
+    B = B.next;
+    if (A.next == null || B.next == null) {
+      return null;
+    }
   }
 }
 
 void main() {
+  Node x = Node(111);
   Node a = Node(3);
   Node b = Node(7);
   Node c = Node(8);
@@ -83,6 +127,7 @@ void main() {
   Node e = Node(99);
   Node f = Node(1);
   // A list
+  x.next = a;
   a.next = b;
   b.next = c;
   c.next = d;
@@ -90,5 +135,5 @@ void main() {
   e.next = f;
   f.next = c;
   // print(d.next);
-  print(getIntersectingNode(a, e).val);
+  print(getIntersectingNode(x, e).val);
 }
